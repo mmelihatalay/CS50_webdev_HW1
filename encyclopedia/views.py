28c52ef01708d995fb1ba3870b9e05f4fa2fd233
Mehmet Melih Atalay <atalay.mehmetmelih@gmail.com>
2021-08-22 15:07:01 +0300

    add search,link for entries

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -4,7 +4,39 @@ from . import util
 
 
 def index(request):
+    entries = util.list_entries()
+
+    if len(request.GET) != 0:
+        checkEntries = [el.lower() for el in entries]
+        if request.GET["q"] in checkEntries:
+            return show(request, request.GET["q"])
+        else:
+            return suggestion(request, request.GET["q"])
+
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": entries,
     })
 
+
+def show(request, name):
+
+    text = util.get_entry(name)
+    if text == None:
+        text = f"{name.upper()} is not in the encyclopedia. Try a different entry!!"
+    return render(request, "encyclopedia/show.html", {
+        "text": text,
+        "name": name
+    })
+
+
+def suggestion(request, name):
+    print(name)
+    entries = util.list_entries()
+    suggestions = []
+    for entry in entries:
+        if name.lower() in entry.lower():
+            suggestions.append(entry)
+
+    return render(request, "encyclopedia/suggestions.html", {
+        "suggestions": suggestions
+    })
